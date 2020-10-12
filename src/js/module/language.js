75e52ff06af3097e9f9e495a63fec936db453887
Huynh Van Tai <taiprogramer@protonmail.com>
2020-10-12 22:24:11 +0700

    [multi-language support: code refactored]
    - Tags: refactor.

--- src/js/module/language.js
+++ src/js/module/language.js
@@ -6,30 +6,23 @@
  */
 
 // contains language for all components
-let languagePackJson = null
 
 async function changeLanguage(countryCode, page) {
   const fileUrl = `lang/${countryCode}.${page}.json`
-  if (languagePackJson === null) {
-    const response = await fetch(fileUrl)
-    if (response.ok) {
-      const json = await response.json()
-      languagePackJson = json
+  const response = await fetch(fileUrl)
+  if (response.ok) {
+    const json = await response.json()
+    for (const key in json) {
+      const element = document.getElementById(key)
+      if (element !== null) {
+        element.innerText = json[key]
+      }
     }
-
-    if (!response.ok) {
-      console.error(`Cannot load data from ${fileUrl}.`)
-      console.log('Make sure your url is correctly.')
-      return
-    }
+    return
   }
 
-  for (const key in languagePackJson) {
-    const element = document.getElementById(key)
-    if (element !== null) {
-      element.innerText = languagePackJson[key]
-    }
-  }
+  console.error(`Cannot load data from ${fileUrl}.`)
+  console.log('Make sure your url is correctly.')
 }
 
 export { changeLanguage }
--- src/js/script.js
+++ src/js/script.js
@@ -1,44 +1,13 @@
 import { changeLanguage } from './module/language.js'
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const fullNameHolder = document.getElementById('fullname-holder'),
-    ageTailHolder = document.getElementById('age-tail-holder'),
-    girlFriendStatusHolder = document.getElementById(
-      'girlfriend-status-holder',
-    ),
-    applyForGirlFriendPositionHolder = document.getElementById(
-      'apply-for-girlfriend-position',
-    ),
-    ageHolder = document.getElementById('age-holder'),
-    infoTitleHolder = document.getElementById('info-title-holder'),
-    contactTitleHolder = document.getElementById('contact-title-holder'),
-    donationTitleHolder = document.getElementById('donation-title-holder'),
-    languageSelector = document.getElementById('language-select'),
-    publicKeyLinkHolder = document.getElementById('public-key-link-holder')
+  const ageHolder = document.getElementById('age-holder'),
+    languageSelector = document.getElementById('language-select')
 
   ageHolder.innerText = new Date().getFullYear() - 2000
-  // fetch language pack
-  const languageObj = await fetch('lang/index.html.json').then((r) => r.json())
-  let languageIndex = 0 // 0 is English, 1 is Vietnamese
 
   languageSelector.onchange = () => {
-    languageIndex = languageSelector.value
-    changeComponentLanguage()
+    const countryCode = languageSelector.value
+    changeLanguage(countryCode, 'index')
   }
-
-  function changeComponentLanguage() {
-    document.title = languageObj.title[languageIndex]
-    fullNameHolder.innerText = languageObj.full_name[languageIndex]
-    ageTailHolder.innerText = languageObj.age_tail[languageIndex]
-    girlFriendStatusHolder.innerText =
-      languageObj.girlfriend_status[languageIndex]
-    applyForGirlFriendPositionHolder.innerText =
-      languageObj.apply_for_girlfriend_position[languageIndex]
-    infoTitleHolder.innerText = languageObj.info_title[languageIndex]
-    contactTitleHolder.innerText = languageObj.contact_title[languageIndex]
-    donationTitleHolder.innerText = languageObj.donation_title[languageIndex]
-    publicKeyLinkHolder.innerText = languageObj.public_key_link[languageIndex]
-  }
-
-  changeLanguage('vi', 'index')
 })
