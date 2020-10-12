8ee3fb824a65edc410e6b476458a468e9cc27bf9
Huynh Van Tai <taiprogramer@protonmail.com>
2020-10-12 22:09:44 +0700

    [#no_stable: remove unused things]
    - (): test.
    - Old language json file.

--- src/js/module/language.js
+++ src/js/module/language.js
@@ -1,17 +1,3 @@
-async function test() {
-  const response = await fetch('lang/index.html.json')
-  if (response.ok) {
-    const json = await response.json()
-    for (const key in json) {
-      const element = document.getElementById(key)
-      if (element === null) {
-        continue
-      }
-      element.innerText = json[key][1]
-    }
-  }
-}
-
 /**
  * Change language
  *
@@ -46,4 +32,4 @@ async function changeLanguage(countryCode, page) {
   }
 }
 
-export { test, changeLanguage }
+export { changeLanguage }
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-import { test, changeLanguage } from './module/language.js'
+import { changeLanguage } from './module/language.js'
 
 document.addEventListener('DOMContentLoaded', async () => {
   const fullNameHolder = document.getElementById('fullname-holder'),
@@ -40,6 +40,5 @@ document.addEventListener('DOMContentLoaded', async () => {
     publicKeyLinkHolder.innerText = languageObj.public_key_link[languageIndex]
   }
 
-  // test()
   changeLanguage('vi', 'index')
 })
